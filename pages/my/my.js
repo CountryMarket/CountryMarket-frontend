f7b478c216d27b254265256181922150c8a03fb0
flyinthesky10 <1276907485@qq.com>
2022-04-30 23:11:59 +0800

    refactored tab pages

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,41 +1,97 @@
 // pages/my/my.js
+
 import { wxLogin } from "../../utils/wxLogin"
 import { wxRequest, isTokenEmpty, validateToken, showTokenInvalidModal } from "../../utils/wxRequest"
 
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-    winHeight: 0
-  },
+Page({
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-    nickName: "你好，请登录",
-    avatarUrl: "https://blog.lyffly.com/static/images/avatar.jpg",
-    isLogging: false,
-    localToken: undefined, // 本地 token，用于和 global 的比较是否有出入
-    isEnter: false,
-    permission: 1,
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        nickName: "你好，请登录",
+        avatarUrl: "https://blog.lyffly.com/static/images/avatar.jpg",
+        isLogging: false,
+        localToken: undefined, // 本地 token，用于和 global 的比较是否有出入
+        permission: 1,
+    },
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-    refresh: function() { // refresh，用于刷新，更新 local token，更新数据
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        wx.setNavigationBarTitle({
+            title: '个人主页'
+        })
+        this.refresh();
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+      // 非第一次进入，判断 token 是否发生变化
       const { globalData } = getApp();
-      this.setData({localToken: globalData.token});
+      validateToken().then(() => { // 检验 Token 合法性, 仅 my 页
+          if (isTokenEmpty(globalData.token)) {
+            showTokenInvalidModal();
+            this.refresh();
+          }
+      }); 
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    },
+
+    // refresh，用于刷新，更新 local token，更新数据
+    refresh: function() { 
+      const { globalData } = getApp();
       // 如果有登录态，获取头像
       if (!isTokenEmpty(globalData.token)) {
          wxRequest("GET", "user/profile").then(res => {
           let porfile = res.data.data;
           this.setData({
             nickName: porfile.nickName,
-            avatarUrl: porfile.avatarUrl
+            avatarUrl: porfile.avatarUrl,
+            permission: porfile.permission
           });
         });
       } else { // 无登录态
@@ -46,108 +102,95 @@ Component({
           permission: 1
         });
       }
-    },
-    onEnter: function() { // 每次 tab 进页面调用
-      if (!this.data.isEnter) { // 第一次进页面 refresh 一次
-        this.setData({ isEnter: true });
-        this.refresh();
-      } else { // 非第一次进入，判断 token 是否发生变化
-        const { globalData } = getApp();
-        validateToken().then(() => { // 检验 Token 合法性, 仅 my 页
-          if (globalData.token != this.data.localToken) {
-            showTokenInvalidModal();
-            this.refresh();
-          }
-        }); 
-      }
-    },
-    userLogin: async function() { // 用户登录，此处 global token 会改变
-      const { globalData } = getApp();
-      console.log(globalData.token)
-      if (isTokenEmpty(globalData.token) && !this.data.isLogging) {
-        this.data.isLogging = true;
-        // 获取昵称头像
-        try {
-          let nickNameTmp, avatarUrlTmp;
-          await new Promise((resolve) => {
-            wx.getUserProfile({
-              desc: '获取你的昵称、头像、地区及性别',
-              success: res => {
-                nickNameTmp = res.userInfo.nickName;
-                avatarUrlTmp = res.userInfo.avatarUrl;
-                console.log(res.userInfo);
-                resolve();
-              },
-              fail: res => {
-                console.log(res)
-                wx.showModal({
-                  title: "提示",
-                  content: "获取头像昵称失败",
-                  showCancel: false,
-                  confirmText: "知道了"
-                });
-                this.data.isLogging = false;
-                return ;
-              }
-            })
-          });
-          wx.showLoading({
-            title: '正在登录中...',
-          });
-          // 获取 code
-          globalData.code = await wxLogin();
-          // console.log(globalData.code)
-          // 将 code 发送到云服务器，获取 token
-          let res = await wxRequest("GET", "user/code", {
-            code: globalData.code,
-            nickName: nickNameTmp,
-            avatarUrl: avatarUrlTmp
-          });
-          console.log(res)
-          // 失败
-          if (!res.data.success) {
-            wx.showModal({
-              title: "提示",
-              content: "登录失败",
-              showCancel: false,
-              confirmText: "知道了"
-            });
-            this.data.isLogging = false;
-            return ;
-          }
-          globalData.token = res.data.data.token;
-          wx.setStorageSync("token", globalData.token); // 两处 token 一起修改
-          this.data.localToken = globalData.token; // 本地同步
+  },
 
-          // 获取头像
-          res = await wxRequest("GET", "user/profile");
-          let profile = res.data.data;
-          if (!res.data.success) { // 获取不到暂时用当前的
-            profile.nickName = nickNameTmp;
-            profile.avatarUrl = avatarUrlTmp;
-          }
-
-          // 更新昵称头像
-          this.setData({
-            nickName: profile.nickName,
-            avatarUrl: profile.avatarUrl,
-            permission: profile.permission,
-          });
-        } catch(err) {
+  // 用户登录，此处 global token 会改变
+  userLogin: async function() {
+    const { globalData } = getApp();
+    console.log(globalData.token)
+    if (isTokenEmpty(globalData.token) && !this.data.isLogging) {
+      this.data.isLogging = true;
+      // 获取昵称头像
+      try {
+        let nickNameTmp, avatarUrlTmp;
+        await new Promise((resolve) => {
+          wx.getUserProfile({
+            desc: '获取你的昵称、头像、地区及性别',
+            success: res => {
+              nickNameTmp = res.userInfo.nickName;
+              avatarUrlTmp = res.userInfo.avatarUrl;
+              console.log(res.userInfo);
+              resolve();
+            },
+            fail: res => {
+              console.log(res)
+              wx.showModal({
+                title: "提示",
+                content: "获取头像昵称失败",
+                showCancel: false,
+                confirmText: "知道了"
+              });
+              this.data.isLogging = false;
+              return ;
+            }
+          })
+        });
+        wx.showLoading({
+          title: '正在登录中...',
+        });
+        // 获取 code
+        globalData.code = await wxLogin();
+        // console.log(globalData.code)
+        // 将 code 发送到云服务器，获取 token
+        let res = await wxRequest("GET", "user/code", {
+          code: globalData.code,
+          nickName: nickNameTmp + "是大十多个使得",
+          avatarUrl: avatarUrlTmp
+        });
+        console.log(res)
+        // 失败
+        if (!res.data.success) {
           wx.showModal({
             title: "提示",
-            content: "登录失败，发生错误",
+            content: "登录失败",
             showCancel: false,
             confirmText: "知道了"
           });
           this.data.isLogging = false;
           return ;
-        } finally {
-          // 关闭 Loading 框
-          wx.hideLoading();
-          this.data.isLogging = false;
         }
+        globalData.token = res.data.data.token;
+        wx.setStorageSync("token", globalData.token); // 两处 token 一起修改
+        this.data.localToken = globalData.token; // 本地同步
+
+        // 获取头像
+        res = await wxRequest("GET", "user/profile");
+        let profile = res.data.data;
+        if (!res.data.success) { // 获取不到暂时用当前的
+          profile.nickName = nickNameTmp;
+          profile.avatarUrl = avatarUrlTmp;
+        }
+
+        // 更新昵称头像
+        this.setData({
+          nickName: profile.nickName,
+          avatarUrl: profile.avatarUrl,
+          permission: profile.permission,
+        });
+      } catch(err) {
+        wx.showModal({
+          title: "提示",
+          content: "登录失败，发生错误",
+          showCancel: false,
+          confirmText: "知道了"
+        });
+        this.data.isLogging = false;
+        return ;
+      } finally {
+        // 关闭 Loading 框
+        wx.hideLoading();
+        this.data.isLogging = false;
       }
     }
-  },
+  }
 })
