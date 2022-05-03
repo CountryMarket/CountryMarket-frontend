2194fff731c35c7797272a6bc1c29c40f3f4382a
MarioChan <1036790957@qq.com>
2022-05-04 02:12:08 +0800

    2022/5/4

--- pages/products/products.js
+++ pages/products/products.js
@@ -1,21 +1,88 @@
 // pages/products/products.js
+
+import { wxLogin } from "../../utils/wxLogin"
+import { wxRequest, isTokenEmpty, validateToken, showTokenInvalidModal ,isResTokenInvalid } from "../../utils/wxRequest"
+
+
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        tabList: [],
+        currentTab: 0,
+        currentId: 0,
+        products: []
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        thiswx.setNavigationBarTitle({
-          title: '商品页'
+        wx.setNavigationBarTitle({
+          title: '商品分类'
         })
+        this.getTabList()
     },
+
+    change_currentTab(e) {
+      console.log(e)
+      this.setData({
+        currentTab: e.currentTarget.dataset.value,
+        currentId: this.data.tabList[ e.currentTarget.dataset.value ].Id
+      })
+      this.gettabProducts()
+    },
+
+    //获取Tab List
+    getTabList() {
+            wx.showLoading({
+                title: '数据加载中'
+            })
+            if (isTokenEmpty(getApp().globalData.token)) {
+              showTokenInvalidModal();
+              wx.hideLoading();
+              return ;
+            }
+            wxRequest("GET","product/tabList").then(res => {
+                  console.log(res)
+                  if (isResTokenInvalid(res)) {
+                    showTokenInvalidModal();
+                    getTabList();
+                    return ;
+                  }
+                  this.setData({
+                      tabList: res.data.data.Tabs,
+                  })
+                  this.setData({
+                    currentId: this.data.tabList[0].Id
+                  })
+            })
+            this.gettabProducts()
+            wx.hideLoading()
+      },
+
+      gettabProducts() {
+        if (isTokenEmpty(getApp().globalData.token)) {
+                  showTokenInvalidModal();
+                  wx.hideLoading();
+                  return;
+        }
+                wxRequest("GET","product/tabProducts",{tabId: this.data.currentId}).then(res => {
+                      console.log(this.data.currentId)
+                      console.log(res)
+                      if (isResTokenInvalid(res)) {
+                        showTokenInvalidModal();
+                        this.gettabProducts();
+                        return ;
+                      }
+                      this.setData({
+                          products: res.data.data.Products
+                      })
+                })
+                console.log(this.data.products)
+      },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
